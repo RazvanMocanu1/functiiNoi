7831141d336c55971ebdb2e2d52edc50403bb600
RazvanMocanu1 <razvanmocanu2009@gmail.com>
2023-09-02 20:46:57 +0300

    inceput filter

--- arraymethods.js
+++ arraymethods.js
@@ -1,7 +1,11 @@
-//ForEach Array Method :
+//ForEach Array Method( + Arrow Function , Lambda Function) :
 
 const lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 lista.forEach((element) => {
-     console.log(element)
+
+     console.log(element + 1 * 15 - 2);
+
 } );
+
+//Chiar si daca fac lista de mii de cifre tot le va pune pe fiecare una cate una in consola.
